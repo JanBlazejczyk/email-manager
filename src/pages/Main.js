744187297d85a3f94d1df3d38933502ca1a2f07e
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2021-11-18 16:27:43 +0100

    main layout done

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,8 @@ import { Dialog } from "../components/Dialog";
 import { AddButton, MainButton } from "../components/Buttons";
 import { useState } from "react";
 
+import "./Main.scss";
+
 
 export function Main() {
   const [addSubscriberFormOpen, setSubscriberFormOpen] = useState(false);
@@ -22,18 +24,24 @@ export function Main() {
   }
 
   return (
-    <div className="main__container">
-      <h2>Email Sender</h2>
-      <MainButton label="Subscribers" />
-      <MainButton label="Campaigns" />
-      <AddButton openDialog={handleSubscriberFormOpen} />
-      <AddButton openDialog={handleCampaignFormOpen} />
-      <Dialog active={addCampaignFormOpen} closeDialog={handleDialogClose}>
-        <AddCampaignForm closeDialog={handleDialogClose} />
-      </Dialog>
-      <Dialog active={addSubscriberFormOpen} closeDialog={handleDialogClose}>
-        <AddSubscriberForm closeDialog={handleDialogClose} />
-      </Dialog>
+    <div className="main">
+      <h2 className="main__title">Email Sender</h2>
+      <div className="main__buttons-container">
+        <MainButton label="Subscribers" />
+        <MainButton label="Campaigns" />
+        <div className="main__buttons-container--add">
+          <AddButton openDialog={handleSubscriberFormOpen} />
+        </div>
+        <div className="main__buttons-container--add">
+          <AddButton openDialog={handleCampaignFormOpen} />
+        </div>
+        <Dialog active={addCampaignFormOpen} closeDialog={handleDialogClose}>
+          <AddCampaignForm closeDialog={handleDialogClose} />
+        </Dialog>
+        <Dialog active={addSubscriberFormOpen} closeDialog={handleDialogClose}>
+          <AddSubscriberForm closeDialog={handleDialogClose} />
+        </Dialog>
+      </div>
     </div>
   );
 }
