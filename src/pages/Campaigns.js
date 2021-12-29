4b9c627aa1ae5bae46e742cb4d71ac4922825253
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2021-12-29 12:24:25 +0100

    added th notification if the list is empty

--- src/pages/Campaigns.js
+++ src/pages/Campaigns.js
@@ -118,18 +118,19 @@ function Campaigns() {
           <MainButton handleClick={displaySent} label="Sent" notActive />
         </div>
         <div className="campaigns__list">
-          {campaigns.filter((campaign) => campaign.fields["Status"] === "Draft").map(campaign => (
-            <div id={campaign.id} key={`${keyValue}-${campaign.id}`}>
-              Subject: {campaign.fields.Subject}<br />
-              Content: {campaign.fields.Content}
-              <div onClick={handleDelete} id={campaign.id}><DeleteButton /></div>
-              <div onClick={handleCampaignFormOpen} id={campaign.id}><EditButton /></div>
-              <div onClick={handleSending} id={campaign.id}><SendButton /></div>
-              <Dialog active={addCampaignFormOpen} closeDialog={handleDialogClose}>
-                <CampaignForm activeId={idToEdit} update={true} subjectContent={defaultSubjectField} emailContent={defaultContentField} greetingContent={defaultGreetingField} closeDialog={handleDialogClose} edit={handleEdit} />
-              </Dialog>
-            </div>
-          ))
+          {campaigns.filter((campaign) => campaign.fields["Status"] === "Draft").length === 0 ? <p>No drafts</p> :
+            campaigns.filter((campaign) => campaign.fields["Status"] === "Draft").map(campaign => (
+              <div id={campaign.id} key={`${keyValue}-${campaign.id}`}>
+                Subject: {campaign.fields.Subject}<br />
+                Content: {campaign.fields.Content}
+                <div onClick={handleDelete} id={campaign.id}><DeleteButton /></div>
+                <div onClick={handleCampaignFormOpen} id={campaign.id}><EditButton /></div>
+                <div onClick={handleSending} id={campaign.id}><SendButton /></div>
+                <Dialog active={addCampaignFormOpen} closeDialog={handleDialogClose}>
+                  <CampaignForm activeId={idToEdit} update={true} subjectContent={defaultSubjectField} emailContent={defaultContentField} greetingContent={defaultGreetingField} closeDialog={handleDialogClose} edit={handleEdit} />
+                </Dialog>
+              </div>
+            ))
           }
         </div>
       </div>
@@ -143,14 +144,15 @@ function Campaigns() {
           <MainButton handleClick={displaySent} label="Sent" />
         </div>
         <div className="campaigns__list">
-          {campaigns.filter((campaign) => campaign.fields["Status"] === "Sent").map(campaign => (
-            <div id={campaign.id} key={`${keyValue}-${campaign.id}`}>
-              Subject: {campaign.fields.Subject}<br />
-              Content: {campaign.fields.Content}
-              Sent: <Moment format="DD.MM.YYYY HH:mm">{Number(campaign.fields.Send)}</Moment>
-              <div onClick={handleDelete} id={campaign.id}><DeleteButton /></div>
-            </div>
-          ))
+          {campaigns.filter((campaign) => campaign.fields["Status"] === "Sent").length === 0 ? <p>No sent campaigns</p> :
+            campaigns.filter((campaign) => campaign.fields["Status"] === "Sent").map(campaign => (
+              <div id={campaign.id} key={`${keyValue}-${campaign.id}`}>
+                Subject: {campaign.fields.Subject}<br />
+                Content: {campaign.fields.Content}
+                Sent: <Moment format="DD.MM.YYYY HH:mm">{Number(campaign.fields.Send)}</Moment>
+                <div onClick={handleDelete} id={campaign.id}><DeleteButton /></div>
+              </div>
+            ))
           }
         </div>
       </div>
